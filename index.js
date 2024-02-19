b4af8984c72de8a0c59a2a587a3dfaa69480f676
ntjson2 <ntjson@gmail.com>
2024-02-18 21:19:43 -0700

    documentation

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const app = express()
 app.set('view engine', 'pug')
 const dotenv = require('dotenv') // Needed to access .env files
 dotenv.config()
-const csvFilePath = 'playerPrevDay.csv'
-const csv = require('csvtojson')
 
-// Home page (just for testing)
-app.get('/', async (req, res) => {
-  const query = await getSportsData();
-  //await axios.get('https://randomuser.me/api/?results=9')
-  console.log(query.response);
-  res.render('index', { spDataApi: query.response})
-})
+const csvFilePath = 'playerPrevDay.csv' 
+const csv = require('csvtojson') // For parsing csv to json
+
 
 // 'sports' page driven by pug template -> sports.pug -> _thumbCard.pug
+// This page calls CVS data and parses it.
 app.get('/sports', async (req, res) => {
   await csv()
     .fromFile(csvFilePath)
@@ -25,9 +20,9 @@ app.get('/sports', async (req, res) => {
     })
 })
 
+
+// Current using CSV data
 function calculateScores (obj) {
-
-
   for (var key in obj) {
     if (typeof obj[key] == 'object') {
       var FTA = parseFloat(obj[key].FTA)
@@ -36,7 +31,6 @@ function calculateScores (obj) {
 
       //Effective field goal percentage
       obj[key].FGP = Math.round(obj[key].FG_PERCENT * 100) // (obj[key].FG + (0.5 * obj[key].threeP))/obj[key].FGA;
-
       obj[key].Usage = Math.round(FTA * 0.44 + FGA + TOV)
 
       //Fantasy Points Per Minute -  FP/Minute
@@ -44,12 +38,14 @@ function calculateScores (obj) {
       const myArray = mins.split(':')
       let minsRounded = myArray[0]
 
+      // Get FPM
       if (obj[key].FG > 0 && minsRounded > 0)
         obj[key].FPM = Math.round((obj[key].FG / minsRounded) * 100) / 100
       else obj[key].FPM = 0
 
       var THREE_PERCENT = parseFloat(obj[key].THREE_PERCENT)
 
+      // Get Three percent
       if (THREE_PERCENT > 0) {
         obj[key].THREE_PERCENT = Math.round(THREE_PERCENT * 100);
       } else {
@@ -58,9 +54,16 @@ function calculateScores (obj) {
 
     }
   }
-
   return obj
 }
+
+
+// Home page - displays API called data (from NBA API)
+app.get('/', async (req, res) => {
+  const query = await getSportsData();
+  console.log(query.response);
+  res.render('index', { spDataApi: query.response})
+})
 
 // Working API with .env key
 const apiKey = process.env.API_KEY
@@ -69,12 +72,13 @@ const options = {
   url: 'https://api-nba-v1.p.rapidapi.com/games',
   params: { season: '2023' },
   headers: {
-    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Key': apiKey, // Stored in .env file
     'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
   }
 }
 
-// API that drives 'sports' page
+
+// API that drives 'sports' page through NBA API
 async function getSportsData () {
   try {
     const response = await axios.request(options)
@@ -84,6 +88,8 @@ async function getSportsData () {
   }
 }
 
+
+// Boilerplate 
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
